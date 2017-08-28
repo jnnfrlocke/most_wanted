10b5566c2018751e927f7bcfcd294488fb940ffe
Jen <jnnfrlocke@gmail.com>
2017-08-28 10:26:12 -0500

    Remove duplicate displayDescendants function

--- app.js
+++ app.js
@@ -140,27 +140,6 @@ function getFamily(person, people) {
         }
     }
 
-    function displayDescendants(person, people) {
-        //this should be called when user enters "descendants"
-        //The last 11 people have parents, so the loop can start at 10 for i
-        //Loops through each object in data array(outter array) and compares "parents" index to "id" of person(inner loop)
-        var kids = [];
-        for (var i = 10; i < people.length; i++) {
-            var parents = people[i].parents //access "parents" array from data.js
-            for (var j = 0; j < parents.length; j++) {
-                if (parents[j] === person.id) {
-                    kids.push(people[i]); //save descendant in array
-                }
-            }
-        }
-        if (kids.length != 0) {
-            displayPeople(kids); //display descendant(s) full name
-            return;
-        } else {
-            alert("This person has no descendants");
-            return;
-        }
-    }
 
     function searchByName(people) {
         var firstName = prompt("What is the person's first name?");
@@ -247,7 +226,8 @@ function getFamily(person, people) {
         <<
         <<
         <<
-        < HEAD
+        <
+        HEAD
         // helper function to pass in as default promptFor validation
         function chars(input) {
             return true; // default validation only
