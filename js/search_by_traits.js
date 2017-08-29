2562f1c7eb688265c09530948e4d63303da6b0b4
jaredburks <jarabu93@gmail.com>
2017-08-29 10:42:33 -0500

    Added weight, occupation, and eyecolor to search criteria.

--- js/search_by_traits.js
+++ js/search_by_traits.js
@@ -16,10 +16,13 @@ function searchByTraits(people){
 		//checkHeight(people);
 		break;
 		case "weight":
+		weightCheck(people);
 		break;
 		case "occupation":
+		occupationCheck(people);
 		break;
 		case "eyecolor":
+		eyecolorCheck(people);
 		break;
 		case "restart":
 		app(people); // restart
@@ -40,4 +43,28 @@ function heightCheck(people){
     return (el.height === enteredHeight);
 	});
 	displayPeople(rightHeight);
+}
+
+function weightCheck(people){
+	var enteredWeight = parseInt(prompt("How much do they weight in pounds?"));
+	var rightWeight = people.filter(function (el) {
+    return (el.weight === enteredWeight);
+	});
+	displayPeople(rightWeight);
+}
+
+function occupationCheck(people){
+	var enteredOccupation = prompt("What is their occupation?");
+	var rightOccupation = people.filter(function (el) {
+    return (el.occupation === enteredOccupation);
+	});
+	displayPeople(rightOccupation);
+}
+
+function eyecolorCheck(people){
+	var enteredEyecolor = prompt("What is their eyecolor?");
+	var rightEyecolor = people.filter(function (el) {
+    return (el.eyeColor === enteredEyecolor);
+	});
+	displayPeople(rightEyecolor);
 }
